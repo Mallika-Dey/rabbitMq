96dc3e1f7ba4ae9049079ed65d1ff2e659ebe836
Mallika-Dey <mallikaju307@gmail.com>
2025-11-09 23:30:26 +0600

    feat: producer consumer with rabbitmq

--- src/main/java/com/example/rabbitmq/RabbitmqApplication.java
+++ src/main/java/com/example/rabbitmq/RabbitmqApplication.java
@@ -1,13 +1,33 @@
 package com.example.rabbitmq;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RabbitmqApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitmqApplication.class, args);
-	}
+    @Profile("usage_message")
+    @Bean
+    public CommandLineRunner usage() {
+        return args -> {
+            System.out.println("This app uses Spring Profiles to its behavior.\n");
+            System.out.println("Sample usage: java -jar rabbit-tutorials.jar --spring.profiles.active=hello-world,sender");
+        };
+    }
+
+    @Profile("!usage_message")
+    @Bean
+    public CommandLineRunner tutorial() {
+        return new RabbitAmqpTutorialsRunner();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/example/rabbitmq/config/Tut1Config.java
@@ -0,0 +1,29 @@
+package com.example.rabbitmq.config;
+
+import com.example.rabbitmq.Tut1Receiver;
+import com.example.rabbitmq.Tut1Sender;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Configuration;
+
+@Profile({"tut1","hello-world"})
+@Configuration
+public class Tut1Config {
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
+    @Profile("receiver")
+    @Bean
+    public Tut1Receiver receiver() {
+        return new Tut1Receiver();
+    }
+
+    @Profile("sender")
+    @Bean
+    public Tut1Sender sender() {
+        return new Tut1Sender();
+    }
+}
