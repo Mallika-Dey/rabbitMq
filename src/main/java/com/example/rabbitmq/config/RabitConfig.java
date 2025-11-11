014b6558f24099f210d9fc62adc09f857142c516
Mallika-Dey <mallikaju307@gmail.com>
2025-11-11 23:10:40 +0600

    feat: work queues

---
+++ src/main/java/com/example/rabbitmq/config/RabitConfig.java
@@ -0,0 +1,37 @@
+package com.example.rabbitmq.config;
+
+import com.example.rabbitmq.Receiver;
+import com.example.rabbitmq.Sender;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"tut2", "work-queues"})
+@Configuration
+public class RabitConfig {
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
+    @Profile("receiver")
+    private static class ReceiverConfig {
+
+        @Bean
+        public Receiver receiver1() {
+            return new Receiver(1);
+        }
+
+        @Bean
+        public Receiver receiver2() {
+            return new Receiver(2);
+        }
+    }
+
+    @Profile("sender")
+    @Bean
+    public Sender sender() {
+        return new Sender();
+    }
+}
